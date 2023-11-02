b5fd90a325cf0701756a573e31ac998605e69a2d
MaksSom <112919332+MaksSom@users.noreply.github.com>
2023-11-02 23:24:47 +0300

    Update programm.cpp

--- programm.cpp
+++ programm.cpp
@@ -14,13 +14,25 @@ vector < pair < string, string > > rewriteRules(string file_text)
 vector < vector < string > > First_k(vector < pair < string, string >> Rules, int k)
 {
 //function first_k
+//visualisation
 }
 
 vector < vector < string > > Follow_k(vector < pair < string, string >> Rules, int k)
 {
 //function follow_k
+//visualisation
 }
 
+vector < vector < char > > LL_1(vector < vector < string > > first, vector < vector < string > > follow)
+{
+
+//visualisation
+}
+
+//function sequenceOfRules
+//Tree
+
+//???
 int main()
 {
 ifstream file("Example.txt");
@@ -40,7 +52,7 @@ for(string a; getline(file, a); )
 vector < pair < string, string > > Rules = rewriteRules(file_text);
 //cin k
 vector < vector < string > > first = First_k(Rules, k);
-vector < vector < string > > first = Follow_k(Rules, k);
+vector < vector < string > > follow = Follow_k(Rules, k);
 
 
 
